efe65a6ef64082f5592b7bf12a28e82399d41073
Johnny <jonysebastin@gmail.com>
2021-11-13 04:43:29 -0800

    Adding cart and plus icon Nov13_1.0

---
+++ components/Cart.tsx
@@ -0,0 +1,14 @@
+import RemoveShoppingCartOutlinedIcon from '@mui/icons-material/RemoveShoppingCartOutlined';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+
+const Cart = () => {
+    return (
+        <div>
+            <RemoveShoppingCartOutlinedIcon/>
+            <ShoppingCartRoundedIcon />
+            
+        </div>
+    );
+};
+
+export default Cart;
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/styled'
 import styles from '../styles/Home.module.css'
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 
 const Menu = ({ menuItems } : any) => {
     return (
     <main className={styles.main}>
-        <Typography component="div" variant="h1">
+        <Typography component="div" variant="h2">
           Coffee Menu
         </Typography>
 
@@ -19,7 +20,8 @@ const Menu = ({ menuItems } : any) => {
                     <Typography variant="subtitle1" color="text.secondary" component="div">
                         {item.itemDescription}
                     </Typography>
-                    <Typography variant="subtitle2" color="text.secondary" component="p">${item.itemTraits?.price}</Typography>
+                    <Typography variant="subtitle2" color="text.secondary" component="span">${item.itemTraits?.price}</Typography>
+                    <AddCircleOutlineRoundedIcon />
                     </MenuCardMUI>
                     ))}
                     </div>                
@@ -35,7 +37,8 @@ const MenuCardMUI = styled(Card)`
   margin-top: 16px;
   padding: 1rem;
   border-radius: 8px;  
-  &:hover{
+  &:hover {
     cursor: pointer;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
   }
 `
