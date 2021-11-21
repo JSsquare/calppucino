35b164d3a34a55c0feb666adf4c66ef12c8995b3
Johnny <jonysebastin@gmail.com>
2021-11-20 21:01:25 -0800

    Copy number button, adding snackbar, Nov20

--- components/WorkInProgress.tsx
+++ components/WorkInProgress.tsx
@@ -1,11 +1,32 @@
+import { forwardRef, useRef, useState } from 'react';
 import styled from '@emotion/styled'
 import Button from '@mui/material/Button';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import WifiCalling3Icon from '@mui/icons-material/WifiCalling3';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { Typography } from '@mui/material';
 import { jelloAnimation } from '../styles/AnimationsStyled';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 const WorkInProgress = () => {
+    const [phNumberCopied, setPhNumberCopied] = useState('')
+    const isNumberCopied = Boolean(phNumberCopied)
+    const phNumberRef = useRef(null);
+
+    const copyPhoneNumber = (e) => {
+        navigator.clipboard.writeText('917-679-1655')
+        setPhNumberCopied('Contact Number Copied')
+    }
+
+    const handleCloseSnackBar = (event: any, reason: any) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setPhNumberCopied('');
+      };
+
     return (
         <WIPContainer>
         <Typography
@@ -15,35 +36,68 @@ const WorkInProgress = () => {
           color="text.primary"
           gutterBottom
         >
-          Website is Work In Progress
+          Website is work-in-progress
           <ConstructionIcon fontSize="large"/>
         </Typography>
-        <Typography variant="h4" align="center" color="text.secondary" component="p" gutterBottom>
-          Call or Text now & pick-up your coffee tomorrow
+        <Typography variant="h5" align="center" color="text.secondary" component="p" gutterBottom>
+          Call or text now & pick-up your 0.99<sup><Typography variant="caption">$</Typography></sup> coffee tomorrow
         </Typography>
 
-        <Typography variant="h6" align="center" color="text.primary" component="p">
+        <Typography 
+            variant="h6" 
+            align="center" 
+            color="text.primary" 
+            component="p" 
+            sx={{ marginTop: 5}}
+            ref={phNumberRef}
+        >
           +1-917-679-1655 
-          <JelloAnimationsWrapper>
-          <Button variant="contained" href="tel:9176791655" style={{marginLeft: 10, marginTop: 30}}>
+        </Typography> 
+        <CTAButtonWrapper>
+        <JelloAnimationsWrapper>
+          <Button variant="contained" href="tel:9176791655" style={{ alignSelf: 'center' }}>
             Call <WifiCalling3Icon />
           </Button>
-          </JelloAnimationsWrapper> 
-        </Typography> 
-                  
+          </JelloAnimationsWrapper>             
+         <Button variant="contained" onClick={copyPhoneNumber} style={{ alignSelf: 'center' }}
+           disabled={isNumberCopied}>
+            {!isNumberCopied && <ContentCopyIcon/>}
+            {isNumberCopied ? 'Copied!' : 'Copy number'}            
+          </Button>
+          </CTAButtonWrapper>   
+
+      <Snackbar open={isNumberCopied} autoHideDuration={6000} onClose={handleCloseSnackBar as any}>
+        <Alert onClose={handleCloseSnackBar} severity="success" sx={{ width: '100%' }}>
+          Copied! Text the number and get your 99c Coffee tomorrow
+        </Alert>
+      </Snackbar>               
         </WIPContainer>
     );
 };
 
+const CTAButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 16px;
+`
+
 const WIPContainer = styled.div`
+    display: flex;
+    flex-direction: column;
     margin-top: 10%;
 `
 const JelloAnimationsWrapper = styled.div`
     animation-duration: .7s;
-    animation-delay: 1.5s;
+    animation-delay: 2s;
+    animation-iteration-count: 2;
     animation-name: ${jelloAnimation};
     transform-origin: center;
+    align-self: center;
 `
 
 export default WorkInProgress;
 
+
+const Alert: any = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref as any} variant="filled" {...props} />;
+});
